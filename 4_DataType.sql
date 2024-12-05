ea482fc467abd2ca7dff80c996b80f5a57bcaf12
Ajay Mali <150048314+ajay-mali27@users.noreply.github.com>
2024-12-05 10:45:24 +0530

    4. Basic Data Types

---
+++ 4_DataType.sql
@@ -0,0 +1,13 @@
+
+CREATE Table dataTypes(
+Name VARCHAR(10),
+id INT,
+phone BIGINT,
+gender CHAR,
+about TEXT(100)
+);
+
+insert into datatypes()
+VALUES("Harry",292,5520903064,'M','Hello.I am Harry,I am a Java Developer.');
+
+SELECT * FROM datatypes;
