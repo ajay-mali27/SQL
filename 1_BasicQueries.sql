534d00286b6f099500aea441fd5d6ad30d6149cb
Ajay Mali <150048314+ajay-mali27@users.noreply.github.com>
2024-12-05 09:52:13 +0530

    1. Basic Queries of SQL

---
+++ 1_BasicQueries.sql
@@ -0,0 +1,14 @@
+--1. Show all the databases;
+SHOW DATABASES;
+--2. Create New Database;
+create DATABASE LokmanyaLibrary;
+
+-- 3. See current Database;
+SELECT DATABASE();
+
+-- 4. Use Database;
+USE LokmanyaLibrary;
+
+-- 5. DELETE DATABASE
+
+DROP DATABASE LokmanyaLibrary;
