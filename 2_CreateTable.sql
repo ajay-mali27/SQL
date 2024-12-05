3fc497db2d84cb234c977bc4a241e205886ec2dc
Ajay Mali <150048314+ajay-mali27@users.noreply.github.com>
2024-12-05 10:10:30 +0530

    2. Table Creation
    Table Creation and Basic of SQL

---
+++ 2_CreateTable.sql
@@ -0,0 +1,44 @@
+SELECT DATABASE();
+
+use Student;
+
+-- Table Creation
+
+create Table MCA (
+    Name VARCHAR(10),
+    Age INT,
+    Marks FLOAT(10),
+    DOB DATE
+)
+-- Insert value in the table;
+-- Date Value insert in this format => 'Year-Month-Date'
+INSERT INTO
+    MCA ()
+VALUES (
+        'John Doe',
+        22,
+        4.5,
+        '2001-03-27'
+    );
+
+INSERT INTO MCA () VALUES ( 'Striver', 23, 32.5, '2002/08/31' );
+
+INSERT INTO MCA () VALUES ('Sara', 22, 98, '2000/01/31');
+
+INSERT INTO
+    MCA ()
+VALUES (
+        'Kalyani',
+        34,
+        68,
+        '2000/02/21'
+    ),
+    (
+        'Prerana',
+        21,
+        87.8,
+        '2001/03/03'
+    );
+
+-- Output
+SELECT * FROM mca;
