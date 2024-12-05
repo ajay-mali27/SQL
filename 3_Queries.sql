7d0e53fa64998f01241945742d0a76afa48e05f2
Ajay Mali <150048314+ajay-mali27@users.noreply.github.com>
2024-12-05 10:20:30 +0530

    3 . Some Most use Queries

---
+++ 3_Queries.sql
@@ -0,0 +1,14 @@
+-- 1. Display all table from the current DATABASE;
+SHOW TABLES;
+
+-- 2. Show all columns from the database
+desc mca;
+
+-- 3. DELETE TABLE FROM Database;
+DROP TABLE mca;
+
+-- 4. Single line comment
+
+/*
+5. Multi line COMMENT
+*/
