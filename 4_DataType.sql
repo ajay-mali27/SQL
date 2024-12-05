cf8ab55e0bc168948a3094bd48ed467753a71665
Ajay Mali <150048314+ajay-mali27@users.noreply.github.com>
2024-12-05 10:57:09 +0530

    Update 4_DataType.sql

--- 4_DataType.sql
+++ 4_DataType.sql
@@ -1,13 +1,38 @@
-
-CREATE Table dataTypes(
-Name VARCHAR(10),
-id INT,
-phone BIGINT,
-gender CHAR,
-about TEXT(100)
+-- Table 1
+CREATE Table dataTypes (
+    Name VARCHAR(10),
+    id INT,
+    phone BIGINT,
+    gender CHAR,
+    about TEXT(100)
 );
 
-insert into datatypes()
-VALUES("Harry",292,5520903064,'M','Hello.I am Harry,I am a Java Developer.');
+insert into
+    datatypes ()
+VALUES (
+        "Harry",
+        292,
+        5520903064,
+        'M',
+        'Hello.I am Harry,I am a Java Developer.'
+    );
 
 SELECT * FROM datatypes;
+
+-- Table 2
+
+CREATE Table Event(
+    Name VARCHAR(10),
+    Event_Date Date,
+    Event_Time TIME
+);
+
+-- Standard Date Formate 'year month date'
+-- 'HH:MM:SS'. (Time)
+INSERT INTO Event() VALUES(
+    'Coldplay',
+    '2024/12/06',
+    '24:56:00'
+);
+
+SELECT * FROM Event;
