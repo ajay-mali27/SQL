d254fbba916ab784dd5c6e2ea47b3c53ff38a0a5
Ajay Mali <150048314+ajay-mali27@users.noreply.github.com>
2024-12-06 12:05:26 +0530

    Delete From command
    It is use to delete the all the rows

---
+++ 8.DeleteFrom.sql
@@ -0,0 +1,20 @@
+DELETE FROM table_name
+WHERE condition;
+
+
+-- Delete From cmd
+
+
+DELETE FROM studentS WHERE Student_ID=1; 
+
+-- Delete all the rows
+DELETE FROM students ;
+
+DELETE FROM students WHERE first_name="Alice";
+
+DELETE FROM students WHERE last_name="Lee";
+
+DELETE FROM students WHERE age=12;
+
+DELETE FROM students WHERE grade="8th";
+SELECT * FROM students;
